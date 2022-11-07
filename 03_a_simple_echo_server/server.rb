3f684f2d4aec40384d934cf5aec607f488b10b00
Andre Light <andrelight.al@gmail.com>
2022-11-07 19:13:42 +0000

    Clarify method, variable, and symbol names
    - Handle more cases in `parse_params` method.

--- 03_a_simple_echo_server/server.rb
+++ 03_a_simple_echo_server/server.rb
@@ -4,14 +4,14 @@ require 'socket'
 
 server = TCPServer.new('ls-ruby-container', 49_152)
 
-def params_from_query_string(query_string)
-  return {} if query_string.nil? || query_string.empty?
+def parse_params(uri_or_query_string)
+  return {} if uri_or_query_string.nil? || uri_or_query_string.empty? ||
+               !uri_or_query_string.include?('=')
 
-  query_string = query_string.slice(1..) if query_string.start_with?('?')
+  uri_or_query_string = uri_or_query_string.split('?')[1] if uri_or_query_string.include?('?')
 
-  param_pairs = query_string.split('&')
-
-  param_pairs.each_with_object({}) do |pair, hash|
+  pairs = uri_or_query_string.split('&')
+  pairs.each_with_object({}) do |pair, hash|
     key, value = pair.split('=')
     hash[key] = value
   end
@@ -22,17 +22,17 @@ def empty_response(client, status: '200 OK')
   client.close
 end
 
-# => [method, path, query_string, http_version]
-def request_start_line_components(start_line)
-  method, request_uri, http_version = start_line.split(' ')
+# => { :method, :path, :params, :version }
+def parse_request_start_line(start_line)
+  method, request_uri, version = start_line.split(' ')
   path, query_string = request_uri.split('?')
 
   { method: method, path: path,
-    params: params_from_query_string(query_string),
-    http_version: http_version }
+    params: parse_params(query_string),
+    version: version }
 end
 
-def process_path_roll(request_components, client)
+def respond_to_path_roll(request_components, client)
   request_params = request_components[:params]
 
   rolls = request_params['count']&.to_i || 1
@@ -45,17 +45,17 @@ def process_path_roll(request_components, client)
   end
 end
 
-def process_path_unknown(request_components, client)
+def respond_to_path_unknown(request_components, client)
   client.puts('HTTP/1.1 404 Not Found')
   client.puts "Content-Type: text/plain\r\n\r\n"
   client.puts "'#{request_components[:path]}' path not found."
 end
 
-def process_path(request_components, client)
+def respond_to_path(request_components, client)
   case request_components[:path]
-  when '/roll' then process_path_roll(request_components, client)
+  when '/roll' then respond_to_path_roll(request_components, client)
   else
-    process_path_unknown(request_components, client)
+    respond_to_path_unknown(request_components, client)
   end
 end
 
@@ -66,8 +66,8 @@ loop do
   next empty_response(client) if !request_start_line || request_start_line =~ /favicon/
 
   puts request_start_line
-  request_components = request_start_line_components(request_start_line)
-  process_path(request_components, client)
+  request_components = parse_request_start_line(request_start_line)
+  respond_to_path(request_components, client)
 
   client.close
 end
