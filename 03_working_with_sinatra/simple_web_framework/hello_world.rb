f6c6b9154e83669678ff83502211e56f163be639
Andre Light <andrelight.al@gmail.com>
2022-11-11 23:14:05 +0000

    Add routes: `/advice` and 404 Not Found

--- 03_working_with_sinatra/simple_web_framework/hello_world.rb
+++ 03_working_with_sinatra/simple_web_framework/hello_world.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
+require_relative 'advice' # loads advice.rb
+
 class HelloWorld
-  def call(_env)
-    ['200', { 'Content-Type' => 'text/plain' }, ['Hello World!']]
+  def call(env)
+    case env['REQUEST_PATH']
+    when '/'
+      ['200', { 'Content-Type' => 'text/plain' }, ['Hello World!']]
+    when '/advice'
+      piece_of_advice = Advice.new.generate    # random piece of advice
+      ['200', { 'Content-Type' => 'text/plain' }, [piece_of_advice]]
+    else
+      [
+        '404',
+        { 'Content-Type' => 'text/plain', 'Content-Length' => '13' },
+        ['404 Not Found']
+      ]
+    end
   end
 end
