6ab702d2f44ed58c01cc43630df37174a46f12ed
Andre Light <andrelight.al@gmail.com>
2022-11-11 23:48:41 +0000

    Refactor `/` index route to ERB view

--- 03_working_with_sinatra/simple_web_framework/hello_world.rb
+++ 03_working_with_sinatra/simple_web_framework/hello_world.rb
@@ -1,21 +1,32 @@
 # frozen_string_literal: true
 
+require 'erb'
 require_relative 'advice' # loads advice.rb
 
 class HelloWorld
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      ['200', { 'Content-Type' => 'text/plain' }, ['Hello World!']]
+      ['200', { 'Content-Type' => 'text/html' },
+       [erb_by_physical_path('views/index.erb').result]]
     when '/advice'
-      piece_of_advice = Advice.new.generate    # random piece of advice
-      ['200', { 'Content-Type' => 'text/plain' }, [piece_of_advice]]
+      piece_of_advice = Advice.new.generate
+      [
+        '200',
+        { 'Content-Type' => 'text/html' },
+        ["<html><body><b><em>#{piece_of_advice}</em></b></body></html>"]
+      ]
     else
       [
         '404',
-        { 'Content-Type' => 'text/plain', 'Content-Length' => '13' },
-        ['404 Not Found']
+        { 'Content-Type' => 'text/html', 'Content-Length' => '48' },
+        ['<html><body><h4>404 Not Found</h4></body></html>']
       ]
     end
+  end
+
+  def erb_by_physical_path(path)
+    template = File.read(path)
+    ERB.new(template)
   end
 end
