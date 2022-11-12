807d2f61d8255cfdaa84c5463609664185659290
Andre Light <andrelight.al@gmail.com>
2022-11-12 23:54:30 +0000

    Add `chapter/:chapter_num` route
    - Fix issue with chapter links: use absolute `href` paths.

--- 03_working_with_sinatra/book_viewer/book_viewer.rb
+++ 03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -2,10 +2,19 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
-TOC = File.read('data/toc.txt').each_line(chomp: true)
+TOC = File.read('data/toc.txt').each_line(chomp: true).to_a
 
 get '/' do
   @title = 'The Adventures of Sherlock Holmes'
   @content_subhead = 'Table of Contents'
   erb :home
+end
+
+get '/chapter/:chapter_num' do |chapter_num|
+  chapter_num = chapter_num.to_i.clamp(1, TOC.size)
+  @chapter_name = "Chapter #{chapter_num} - #{TOC[chapter_num - 1]}"
+  @chapter_paragraphs = File.read("data/chp#{chapter_num}.txt").each_line('', chomp: true)
+  @title = "The Adventures of Sherlock Holmes - #{@chapter_name}"
+  @content_subhead = @chapter_name
+  erb :chapter
 end
