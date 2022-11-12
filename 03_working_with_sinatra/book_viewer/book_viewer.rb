363234ec0cbcaec6bbb76a802c46943e75176e60
Andre Light <andrelight.al@gmail.com>
2022-11-12 03:35:38 +0000

    Refactor static template HTML to ERB template

--- 03_working_with_sinatra/book_viewer/book_viewer.rb
+++ 03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -1,6 +1,8 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
-get "/" do
-  File.read "public/template.html"
+get '/' do
+  @value = 'My dynamic value'
+  erb :home
 end
