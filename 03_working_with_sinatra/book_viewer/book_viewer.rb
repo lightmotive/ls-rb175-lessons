c695349e24428bde3bf02593ab708d65bf7f6df6
Andre Light <andrelight.al@gmail.com>
2022-11-13 00:33:21 +0000

    Highlight selected chapter in menu

--- 03_working_with_sinatra/book_viewer/book_viewer.rb
+++ 03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -11,9 +11,9 @@ get '/' do
 end
 
 get '/chapter/:chapter_num' do |chapter_num|
-  chapter_num = chapter_num.to_i.clamp(1, TOC.size)
-  @chapter_name = "Chapter #{chapter_num} - #{TOC[chapter_num - 1]}"
-  @chapter_paragraphs = File.read("data/chp#{chapter_num}.txt").each_line('', chomp: true)
+  @chapter_num = chapter_num.to_i.clamp(1, TOC.size)
+  @chapter_name = "Chapter #{@chapter_num} - #{TOC[@chapter_num - 1]}"
+  @chapter_paragraphs = File.read("data/chp#{@chapter_num}.txt").each_line('', chomp: true)
   @title = "The Adventures of Sherlock Holmes - #{@chapter_name}"
   @content_subhead = @chapter_name
   erb :chapter
