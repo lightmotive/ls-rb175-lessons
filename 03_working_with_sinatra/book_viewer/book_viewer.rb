8e2d332b24328dc98e21266312b38ddb2180fc80
Andre Light <andrelight.al@gmail.com>
2022-11-13 03:24:15 +0000

    Refactor common router logic to `before` filter

--- 03_working_with_sinatra/book_viewer/book_viewer.rb
+++ 03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -4,28 +4,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 
-def toc_strings
-  File.read('data/toc.txt').each_line(chomp: true).to_a
+before do
+  @title = 'The Adventures of Sherlock Holmes'
+  @toc_strings = File.read('data/toc.txt').each_line(chomp: true).to_a
 end
 
 get '/' do
-  @toc_strings = toc_strings
-
-  @title = 'The Adventures of Sherlock Holmes'
   @content_subhead = 'Table of Contents'
 
   erb :home
 end
 
 get '/chapter/:number' do |number|
-  @toc_strings = toc_strings
-
   @chapter_num = number.to_i.clamp(1, @toc_strings.size)
   @chapter_name = "Chapter #{@chapter_num} - #{@toc_strings[@chapter_num - 1]}"
   @chapter_paragraphs = File.read("data/chp#{@chapter_num}.txt")
                             .each_line('', chomp: true)
 
-  @title = "The Adventures of Sherlock Holmes - #{@chapter_name}"
+  @title += " - #{@chapter_name}"
   @content_subhead = @chapter_name
 
   erb :chapter
