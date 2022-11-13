2226984e21628576b96742992014a12e9894b9e2
Andre Light <andrelight.al@gmail.com>
2022-11-13 03:09:37 +0000

    Clarify `/chapter/` route code

--- 03_working_with_sinatra/book_viewer/book_viewer.rb
+++ 03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -12,11 +12,14 @@ get '/' do
   erb :home
 end
 
-get '/chapter/:chapter_num' do |chapter_num|
-  @chapter_num = chapter_num.to_i.clamp(1, TOC.size)
+get '/chapter/:number' do |number|
+  @chapter_num = number.to_i.clamp(1, TOC.size)
   @chapter_name = "Chapter #{@chapter_num} - #{TOC[@chapter_num - 1]}"
-  @chapter_paragraphs = File.read("data/chp#{@chapter_num}.txt").each_line('', chomp: true)
+  @chapter_paragraphs = File.read("data/chp#{@chapter_num}.txt")
+                            .each_line('', chomp: true)
+
   @title = "The Adventures of Sherlock Holmes - #{@chapter_name}"
   @content_subhead = @chapter_name
+
   erb :chapter
 end
