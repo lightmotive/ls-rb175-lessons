35fb9eb7dcdccba6d8f1a9bf369d016550667520
Andre Light <andrelight.al@gmail.com>
2022-11-15 15:04:38 +0000

    Implement user stats feature

--- 03_working_with_sinatra/users_and_interests/users_and_interests.rb
+++ 03_working_with_sinatra/users_and_interests/users_and_interests.rb
@@ -5,9 +5,23 @@ require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'yaml'
 
+def calculate_user_stats
+  interest_count = @users.values.reduce(0) do |sum, user_detail|
+    sum + user_detail[:interests].size
+  end
+  @user_stats = { count: @users.keys.size, interest_count: interest_count }
+end
+
 before do
-  @title = 'Users with Interests'
+  @title = '* Users *'
   @users = YAML.load_file('data/users.yaml')
+end
+
+helpers do
+  def user_stats
+    calculate_user_stats
+    "There are #{@user_stats[:count]} users with a total of #{@user_stats[:interest_count]} interests."
+  end
 end
 
 get '/' do
